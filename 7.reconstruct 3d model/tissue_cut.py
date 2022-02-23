070d297ee36739238fb09558f7daa2bc6a20069a
Jiajun Yao <yjj_zj@163.com>
2022-02-23 14:18:33 +0800

    upload

--- 7.reconstruct 3d model/tissue_cut.py
+++ 7.reconstruct 3d model/tissue_cut.py
@@ -18,7 +18,7 @@ adata.obs["y"] = adata.obs["y"] - adata.obs["y"].min()
 adata.obs["z"] = adata.obs["z"] - 4.9
 adata.obs[["x", "y", "z"]] = adata.obs[["x", "y", "z"]].round(2)
 adata.obsm["spatial"] = adata.obs[["x", "y", "z"]].values
-arr = adata.obs[["x", "y", "z"]].values
+
 
 clusters = ["fat body", "CNS"]
 clusters_color = ["#F56867", "#FEB915"]
@@ -31,7 +31,6 @@ for cluster, color in zip(clusters, clusters_color):
                                              mask_alpha=0, surf_alpha=0.5, surf_color=color,
                                              n_surf=10000, voxel_smooth=200, voxelize=True, voxel_size=[0.5, 0.5, 0.7])
     sub_meshes.append(sub_mesh)
-
 
 mesh = sd.tl.build_three_d_model(adata=adata, groupby='anno', group_show='all', surf_alpha= 0.2, group_amap=0,
                                  n_surf=10000, voxel_smooth=200, voxelize=True, voxel_size=[0.5, 0.5, 0.7])
